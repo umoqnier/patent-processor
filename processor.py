cc91976c926fe95050cdc4725bcf2bc5b514e173
Diego Alberto Barriga Martínez <umoqnier@gmail.com>
2018-06-01 14:54:14 -0500

    Agregando patentes y esquelo del procesador de patentes

---
+++ processor.py
@@ -0,0 +1,17 @@
+import xml.etree.ElementTree as ET
+
+
+def get_xml_file(year, month):
+    base = "PA_RE_" + year + '_' + month + "_001.xml"
+
+
+def get_fields_from_xml(data):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
